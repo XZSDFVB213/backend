e30beb66a10ca5112a1f90eb73a544a67647a251
XZSDFVB213 <abakarmadatov2@gmail.com>
2026-05-04 14:35:47 +0300

    add user consents (policy + ter

--- prisma/migrations/20260504111059_add_consents_tabl/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `acceptedAt` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "acceptedAt",
-ADD COLUMN     "acceptedAtTime" TIMESTAMP(3);
