d2865f93908bfbc946899cd833de2b747b04acfc
XZSDFVB213 <abakarmadatov2@gmail.com>
2026-05-26 14:25:24 +0300

    fix frontol providers

--- src/frontol/frontol.module.ts
+++ src/frontol/frontol.module.ts
@@ -3,11 +3,13 @@ import { Module } from '@nestjs/common';
 import { FrontolController } from './frontol.controller';
 import { FrontolService } from './frontol.service';
 import { NotificationModule } from '../notification/notification.module'; // важно!
+import { NotificationService } from 'src/notification/notification.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   imports: [NotificationModule],
   controllers: [FrontolController],
-  providers: [FrontolService],
+  providers: [FrontolService, NotificationService, PrismaService],
   exports: [FrontolService],
 })
 export class FrontolModule {}
